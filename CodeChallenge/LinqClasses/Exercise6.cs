73cfad3e3a186573f1fef9ee99f3e3c8cc9c8287
Claudia <claudia.jara@asamblo.com>
2025-05-10 09:32:37 -0300

    3.LINQ and collections: Return only unique values from a collection of complex objects:

---
+++ CodeChallenge/LinqClasses/Exercise6.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+
+namespace LinqClasses
+{
+    public class Exercise6 : IExercise
+    {
+        public List<Student> Students;
+        public string Execute()
+        {
+            var uniqueNames = Students.DistinctBy(x => x.Name).Select(x => x.Name);
+
+            return ShowData(uniqueNames);
+        }
+
+        public void PrepareData()
+        {
+            Students = new List<Student>
+            {
+                new Student
+                {
+                    Name = "Ana",
+                    Grade = "A",
+                    GPA = 3.5
+                },
+                new Student
+                {
+                    Name = "Ana",
+                    Grade = "B",
+                    GPA = 2.5
+                },
+                new Student
+                {
+                    Name = "Bob",
+                    Grade = "A",
+                    GPA = 3.5
+                },
+                new Student
+                {
+                    Name = "Dylan",
+                    Grade = "B",
+                    GPA = 3.5
+                },
+            };
+        }
+
+        public string ShowData(object objectToParse)
+        {
+            return JsonSerializer.Serialize(objectToParse);
+        }
+    }
+}
