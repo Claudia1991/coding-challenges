8378de8b2821213a2b9e9182e547b76f75add76d
Claudia <claudia.jara@asamblo.com>
2025-05-11 20:13:57 -0300

    5. Multithreading / Asynchronous Programming: Implement a simple background worker using `Task.Run()`

---
+++ CodeChallenge/MultithreadingClasses/Exercise3.cs
@@ -0,0 +1,38 @@
+﻿namespace MultithreadingClasses
+{
+    public class Exercise3
+    {
+        public async Task<string> BackgroundWorker(Func<Task> longTask)
+        {
+			try
+			{
+                await Task.Run(longTask);
+                return "Works!";
+			}
+			catch (Exception)
+			{
+                return "Something wrong happen";
+			}
+        }
+    }
+
+    public class PrincipalWorker
+    {
+        public async Task RunExample()
+        {
+            Exercise3 exercise3 = new Exercise3();
+
+            Func<Task> longTask = async () =>
+            {
+                Console.WriteLine("Start background");
+                await Task.Delay(3000);
+                Console.WriteLine("Finish background");
+            };
+
+            Console.WriteLine("Main thread finished");
+            
+            var result = await exercise3.BackgroundWorker(longTask);
+            Console.WriteLine("Result background worker: " + result);
+        }
+    }
+}
