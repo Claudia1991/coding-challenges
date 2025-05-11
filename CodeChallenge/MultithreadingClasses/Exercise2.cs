884d7ad17a1f934a8783a25ace160388bc81893d
Claudia <claudia.jara@asamblo.com>
2025-05-11 16:19:22 -0300

    5. Multithreading / Asynchronous Programming: Write a thread-safe singleton class

---
+++ CodeChallenge/MultithreadingClasses/Exercise2.cs
@@ -0,0 +1,51 @@
+﻿namespace MultithreadingClasses
+{
+    public class Singleton
+    {
+        private static Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton Instance => _instance.Value;
+
+        private Singleton()
+        {
+            Console.WriteLine("Constructor");
+        }
+
+        public static void DoSomething()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+
+    public class Singleton2
+    {
+        private static object _lock = new object();
+        private static readonly Singleton2 _instance;
+        public static Singleton2 Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            return new Singleton2 ();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private Singleton2()
+        {
+            Console.WriteLine("Constructor");
+        }
+
+        public static void DoSomething()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+}
